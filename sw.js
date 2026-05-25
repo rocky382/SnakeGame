cdd14d051442393177c442f39f72266a5ac06eb6
rocky382 <chembur533@gmail.com>
2026-05-25 20:59:22 +0400

    Add service worker for offline support

---
+++ sw.js
@@ -0,0 +1,42 @@
+// Service Worker for offline support
+const CACHE_NAME = 'neon-snake-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon-512.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+      .catch(() => {
+        if (event.request.destination === 'document') {
+          return caches.match('./index.html');
+        }
+      })
+  );
+});
